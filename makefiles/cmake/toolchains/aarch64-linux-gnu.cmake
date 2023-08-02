bc9fe381c1db2d670ef1a10371b2134efb974702
ykla <yklaxds@gmail.com>
2023-08-02 13:18:55 +0800

    1

--- makefiles/cmake/toolchains/aarch64-linux-gnu.cmake
+++ makefiles/cmake/toolchains/aarch64-linux-gnu.cmake
@@ -9,8 +9,8 @@ SET(CMAKE_CXX_COMPILER aarch64-linux-gnu-g++)
 SET(CMAKE_ASM_COMPILER aarch64-linux-gnu-gcc)
 SET(CMAKE_SYSTEM_PROCESSOR arm)
 
-#ADD_DEFINITIONS("-march=armv6")
-#add_definitions("-mcpu=arm1176jzf-s -mfpu=vfp -mfloat-abi=hard -marm")
+#ADD_DEFINITIONS("-march=aarch64")
+#add_definitions("-mfpu=vfp -mfloat-abi=hard -marm")
 
 # rdynamic means the backtrace should work
 IF (CMAKE_BUILD_TYPE MATCHES "Debug")
